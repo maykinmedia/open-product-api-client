9f191b6274de2d97cfdaa958d36abb942db4681f
maykin <thomas@maykinmedia.nl>
2025-04-29 17:44:32 +0200

    Added conversion of python lists to api-compatible

--- openproductapiclient/utils.py
+++ openproductapiclient/utils.py
@@ -1,11 +1,17 @@
 def get_filled_params(rawparams):
     """
     Takes in a functions 'raw params' (e.g. retrieved with 'locals()')
-    and removes all the 'self' and None-valued entries.
+    and removes all the 'self' and None-valued entries. Also converts 
+    lists into a format understood by the API.
     """
 
+    # i = []
+    # for key, value in rawparams.items():
+    #     if isinstance(value, list[str]):
+    #         value = ', '.join(value)
+
     return {
-        key: value
+        key: ', '.join(value) if isinstance(value, list) else value
         for key, value in rawparams.items()
         if key != "self" and value is not None
     }
